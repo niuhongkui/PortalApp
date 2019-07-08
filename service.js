59095e5f77ffa787c183f199a6aef14f1073f3aa
niuhongkui <397208778@qq.com>
2019-07-08 22:34:48 +0800

    跨域

--- service.js
+++ service.js
@@ -2,25 +2,39 @@
 const USERS_KEY = 'USERS_KEY';
 const STATE_KEY = 'STATE_KEY';
 
-const getUsers = function () {
-    let ret = '';
-    ret = uni.getStorageSync(USERS_KEY);
-    if (!ret) {
-        ret = '[]';
-    }
-    return JSON.parse(ret);
+var url = "http://127.0.0.1/api/userinfo/loginon/1"
+
+const getUsers = function(userinfo) {
+	uni.request({
+		url: url, //仅为示例，并非真实接口地址。
+		data: userinfo,
+		header:{
+			'content-type':'application/json'
+		},
+		dataType: 'json',
+		method :'POST',
+		success: (res) => {
+			debugger
+		}
+	});
+	let ret = '';
+	ret = uni.getStorageSync(USERS_KEY);
+	if (!ret) {
+		ret = '[]';
+	}
+	return JSON.parse(ret);
 }
 
-const addUser = function (userInfo) {
-    let users = getUsers();
-    users.push({
-        account: userInfo.account,
-        password: userInfo.password
-    });
-    uni.setStorageSync(USERS_KEY, JSON.stringify(users));
+const addUser = function(userInfo) {
+	let users = getUsers(userInfo);
+	users.push({
+		UserCode: userInfo.UserCode,
+		PassWord: userInfo.PassWord
+	});
+	uni.setStorageSync(USERS_KEY, JSON.stringify(users));
 }
 
 export default {
-    getUsers,
-    addUser
+	getUsers,
+	addUser
 }
