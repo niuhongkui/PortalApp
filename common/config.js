75fdb3cae320c86a60d84edcb1333fe60e6d57c7
niuhongkui <397208778@qq.com>
2019-07-26 16:03:46 +0800

    文件添加

---
+++ common/config.js
@@ -0,0 +1,9 @@
+//请求地址
+const url="http://localhost:81";
+//缓存名称的key
+const userKey='App_User_Key';
+
+module.exports={
+	url:url,
+	userKey:userKey
+}
--- store/index.js
+++ store/index.js
@@ -4,25 +4,32 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
-    state: {
-        /**
-         * 是否需要强制登录
-         */
-        forcedLogin: false,
-        hasLogin: false,
-        userName: ""
-    },
-    mutations: {
-        login(state, userName) {
-			debugger
-            state.userName = userName || '新用户';
-            state.hasLogin = true;
-        },
-        logout(state) {
-            state.userName = "";
-            state.hasLogin = false;
-        }
-    }
+	state: {
+		hasLogin: false,
+		userInfo: {},
+	},
+	mutations: {
+		login(state, provider) {
+
+			state.hasLogin = true;
+			state.userInfo = provider;
+			uni.setStorage({//缓存用户登陆状态
+			    key: 'userInfo',  
+			    data: provider  
+			}) 
+			console.log(state.userInfo);
+		},
+		logout(state) {
+			state.hasLogin = false;
+			state.userInfo = {};
+			uni.removeStorage({  
+                key: 'userInfo'  
+            })
+		}
+	},
+	actions: {
+	
+	}
 })
 
 export default store
