162f02cf68c4bd891f87c157fe0765f0cf084ef5
niuhongkui <397208778@qq.com>
2020-08-27 09:53:58 +0800

    手机验证

--- common/util.js
+++ common/util.js
@@ -68,7 +68,7 @@ var dateUtils = {
 };
 
 function checkPhone(str){
-	var myreg = /^[1][3,4,5,7,8,9][0-9]{9}$/;
+	var myreg = /^[1][3,4,5,6,7,8,9][0-9]{9}$/;
 	if (!myreg.test(str)) {
 		return false;
 	} else {
