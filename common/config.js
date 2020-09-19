3742442cd90ede6d39e34482872fa4e91de8dacd
niuhongkui <397208778@qq.com>
2020-09-19 22:31:30 +0800

    yunming

--- common/config.js
+++ common/config.js
@@ -1,9 +1,9 @@
 //请求地址
- const url= "http://49.232.204.122/";
+ const url= "http://dev.sjzyimin.com/";
 //缓存名称的key
 const userKey='App_User_Key';
 //版本信息
-const version='V1.02';
+const version='1.02';
 
 module.exports={
 	url:url,
